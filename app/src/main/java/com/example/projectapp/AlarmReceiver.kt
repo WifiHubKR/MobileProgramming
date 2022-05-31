00b66eeda5a816690202c0a68a942931044f3d57
WifiHub <kimjh456789@gmail.com>
2022-05-31 20:20:17 +0900

    알람, DB 테이블 완성

---
+++ app/src/main/java/com/example/projectapp/AlarmReceiver.kt
@@ -0,0 +1,58 @@
+package com.example.projectapp
+
+import android.R
+import android.app.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.getSystemService
+
+
+class AlarmReceiver : BroadcastReceiver() {
+    var manager: NotificationManager? = null
+    var builder: NotificationCompat.Builder? = null
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channelID = "MyChannelWeek"
+            val channelName = "week"
+            val notificationChannel = NotificationChannel(channelID, channelName,
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.enableVibration(true)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            val builder = NotificationCompat.Builder(context, channelID)
+                .setContentTitle("술약속이 있으신가요?")
+                .setContentText("접속하셔서 일정을 기록하고 목표를 달성해보세요!")
+                .setSmallIcon(com.example.projectapp.R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            manager.createNotificationChannel(notificationChannel)
+
+            val notification = builder.build()
+            manager.notify(10, notification)
+            Log.v("alarm", "Week 알람 발생")
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/projectapp/DBHelper.kt
@@ -0,0 +1,84 @@
+package com.example.projectapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import java.util.*
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
+
+
+//context 정보는 멤버로 많이 사용하므로 반드시 생성자에 있어야 하고 나머지는 클래스 내부에 Static 멤버로 만들면 된다.
+
+class DBHelper(val context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
+
+    // 자바에서 Static과 동일한 역할. 멤버 변수 설정
+    companion object {
+        val DB_NAME = "mydb.db"
+        val DB_VERSION = 1
+        val TABLE_NAME = "products"
+        val DATE = "date"
+        val PNAME = "pname"
+        val PQUANTITY = "pquantity"
+        val DAILY_GOAL = "daily_goal"
+        val WEEKLY_GOAL = "weekly_goal"
+    }
+
+    //DB가 생성될 때 실행되는 함수
+    override fun onCreate(db: SQLiteDatabase?) {
+        //table 생성 sql문 생성
+        val create_table = "create table if not exists $TABLE_NAME(" +
+                "$DATE date primary key autoincrement," +
+                "$PNAME text, " +
+                "$PQUANTITY integer," +
+                "$DAILY_GOAL integer," +
+                "$WEEKLY_GOAL integer);"
+
+        //생성한 sql문을 DB 객체를 이용해 전송
+        db!!.execSQL(create_table)
+    }
+
+    //데이터베이스의 버전이 바뀐 경우 호출하는 함수
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        val drop_table = "drop table if exists $TABLE_NAME;"
+
+        db!!.execSQL(drop_table)
+        onCreate(db)
+    }
+
+    // DB에 값을 넣어주는 함수 생성
+    fun insertGoal(daily_goal : Int, weekly_goal : Int):Boolean{
+        val values = ContentValues()
+        values.put(DAILY_GOAL, daily_goal)
+        values.put(WEEKLY_GOAL, weekly_goal)
+        val db = writableDatabase
+        if(db.insert(TABLE_NAME, null, values)>0){
+            db.close()
+            return true
+        }
+        else{
+            db.close()
+            return false
+        }
+
+    }
+
+    fun updateGoal(date: Date, daily_goal: Int, weekly_goal: Int):Boolean{
+        val strsql = "select * from $TABLE_NAME where $DATE = '$date';"
+        val db = writableDatabase
+        val cursor = db.rawQuery(strsql, null)
+        val flag = cursor.count!=0
+        if(flag){
+            cursor.moveToFirst()
+            val values = ContentValues()
+            values.put(DAILY_GOAL ,daily_goal)
+            values.put(WEEKLY_GOAL, weekly_goal)
+            db.update(TABLE_NAME, values, "$DATE = ?", arrayOf(date.toString()))
+        }
+        //작업 완료 후 db, cursor를 닫아주는 함수 호출
+        cursor.close()
+        db.close()
+        return flag
+    }
+
+
+}
